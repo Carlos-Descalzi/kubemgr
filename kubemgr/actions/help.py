2c69ef2c85c0a68c1fa9b7114fe5dcd0d2aa1fb3
Carlos Descalzi <carlos.descalzi@gmail.com>
2020-09-13 10:50:22 -0300

    More refactoring + added missing requirement

---
+++ kubemgr/actions/help.py
@@ -0,0 +1,8 @@
+from kubemgr.texts import HELP_CONTENTS
+
+class ShowHelp:
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, *_):
+        self.app.show_text_popup(HELP_CONTENTS.split("\n"))
